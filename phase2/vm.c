fbfc8f1e59747b93b8b46a0473ec0a568a895169
rojojun <zoloman316@gmail.com>
2026-03-24 22:57:55 +0900

    phase2 미션2

--- phase2/vm.c
+++ phase2/vm.c
@@ -19,17 +19,20 @@ int main(void)
 {
     // "int a=1; int b=2; int c=a+b;" 의 바이트코드
     unsigned char code[] = {
-        0x04, // iconst_1
-        0x3C, // istore_1
+        0x08, // iconst_5
         0x05, // iconst_2
-        0x3D, // istore_2
-        0x1B, // iload_1
-        0x1C, // iload_2
-        0x60, // iadd
-        0x3E, // istore_3
+        0x64, // isub       → 5 - 2 = 3
+        0x06, // iconst_3
+        0x68, // imul       → 3 * 3 = 9
+        0x04, // iconst_1
+        0x6C, // idiv       → 9 / 1 = 9
+        0x3C, // istore_1
     };
     int code_length = 8;
     int locals[256] = {0};
+
+    int a = 0;
+    int b = 0;
 
     for (int pc = 0; pc < code_length; pc++)
     {
@@ -40,6 +43,12 @@ int main(void)
             break;
         case 0x05:
             push(2);
+            break;
+        case 0x06:
+            push(3);
+            break;
+        case 0x08:
+            push(5);
             break;
         case 0x3C:
             locals[1] = pop();
@@ -59,16 +68,22 @@ int main(void)
         case 0x60:
             push(pop() + pop());
             break;
+        case 0x64:
+            a = pop();
+            b = pop();
+            push(b - a);
+            break;
+        case 0x68:
+            push(pop() * pop());
+            break;
+        case 0x6C:
+            a = pop();
+            b = pop();
+            push(b / a);
+            break;
         default:
             break;
         }
     }
-    printf("c = %d\n", locals[3]);
-    // push(1);
-    // push(2);
-    // int a = pop();
-    // int b = pop();
-    // push(a + b);
-    // printf("result: %d\n", pop());
-    // return 0;
+    printf("c = %d\n", locals[1]);
 }
